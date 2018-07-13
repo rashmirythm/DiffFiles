f4a63d15d792fa08bc5ffa842d740ee741c86617
i338008 <rashmi.r02@sap.com>
2018-07-13 16:19:15 +0530

    initial commit with .java, .js and .properties files

---
+++ JavascriptFile.js
@@ -0,0 +1,28 @@
+
+	function add(a, b) {
+	    return a + b;            // Function returns the product of a and b
+	}
+
+	function sub(a,b)
+	{
+		return a - b;
+		}
+
+	function pro(a,b)
+	{
+		return a * b;
+		}
+
+	function quo(a,b)
+	{
+		return a / b;
+		}
+
+	function mod(a,b)
+	{
+		return a % b;
+		}
+	
+	
+
+
